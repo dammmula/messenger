6314fddee308bd45b57b7534f8adf18ea4493ce9
dammmula <eliza04102001@mail.com>
2021-03-29 22:13:21 +0300

    Fix bugs

--- src/Components/Chat/Chat.js
+++ src/Components/Chat/Chat.js
@@ -1,25 +1,11 @@
 import React from 'react';
-<<<<<<< HEAD
-<<<<<<< HEAD
 
 import './Chat.css';
 
 import InputPanel from "../InputPanel/InputPanel";
 import Messages from "../Messages/Messages";
-=======
-=======
 
->>>>>>> 743f4cf... Add Messages componenet
-import './Chat.css';
-
-import InputPanel from "../InputPanel/InputPanel";
-<<<<<<< HEAD
->>>>>>> afb5b5f... Add project
-=======
-import Messages from "../Messages/Messages";
->>>>>>> 743f4cf... Add Messages componenet
-
-
+//TODO fix scrollbar position underneath when new messages are added
 export default class Chat extends React.Component {
     state = {
         messages: [
@@ -28,60 +14,31 @@ export default class Chat extends React.Component {
             this.createMessage('Naruto Uzumaki', "I will never give up or go back on my word, because that's my ninja way!", '/images/naruto.jpeg', false),
             this.createMessage('Sakura Haruno', "*facepalm*", '/images/sakura.jpg', false),
             this.createMessage('Sasuke Uchiha', "here we go again", '/images/sasuke.jpeg', false),
-<<<<<<< HEAD
-<<<<<<< HEAD
         ],
         inputText: '',
         messageToEditId: null
-=======
-        ]
->>>>>>> afb5b5f... Add project
-=======
-        ],
-        inputText: '',
-        messageToEditId: null
->>>>>>> 0ce8f22... Add editing message functionality
     }
 
     messageIds = 0
 
     setTime() {
         let now = new Date();
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 0ce8f22... Add editing message functionality
         let hours = (String(now.getHours()).length === 1) ?
             '0' + now.getHours() : now.getHours();
         let minutes = (String(now.getMinutes()).length === 1) ?
             '0' + now.getMinutes() : now.getMinutes();
-<<<<<<< HEAD
-=======
-        let hours = (String(now.getHours()).length === 1) ? '0' + now.getHours() : now.getHours();
-        let minutes = (String(now.getMinutes()).length === 1) ? '0' + now.getMinutes() : now.getMinutes();
->>>>>>> afb5b5f... Add project
-=======
->>>>>>> 0ce8f22... Add editing message functionality
         return hours + ':' + minutes;
     }
 
     createMessage (name, text, image, ownMessage = true) {
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
->>>>>>> afb5b5f... Add project
-=======
->>>>>>> 0ce8f22... Add editing message functionality
         return {
             name,
             text,
             image,
             ownMessage,
             id: this.messageIds++,
-            time: this.setTime()
-<<<<<<< HEAD
-<<<<<<< HEAD
+            time: this.setTime(),
+            edited: false
         };
     }
 
@@ -111,51 +68,10 @@ export default class Chat extends React.Component {
                 messageToEditId: id
             };
         });
-=======
-        }
-=======
-        };
->>>>>>> 0ce8f22... Add editing message functionality
-    }
-
-    sendMessage = (text) => {
-        this.setState(({ messages }) => {
-            return {messages: [...messages,
-                this.createMessage('You', text, '/images/kakashi.png')
-                ]};
-        });
-    }
-
-    onTextSubmit = (event) => {
-        event.preventDefault();
-        this.sendMessage(this.state.inputText);
-        this.setState({ inputText: '' });
-    }
-
-<<<<<<< HEAD
->>>>>>> afb5b5f... Add project
-=======
-    onTextInput = (event) => {
-        this.setState({ inputText: event.target.value });
-    }
-
-    inputMessageToEdit = (id) => {
-        this.setState(({ messages }) => {
-            let message = messages.find((item) => item.id === id);
-            return {
-                inputText: message.text,
-                messageToEditId: id
-            };
-        });
->>>>>>> 0ce8f22... Add editing message functionality
     }
 
     deleteMessage = (id) => {
         this.setState(({ messages }) => {
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 0ce8f22... Add editing message functionality
             if (id === this.state.messageToEditId) {
                 return {
                     messages: messages.filter((item) => item.id !== id),
@@ -164,7 +80,6 @@ export default class Chat extends React.Component {
                 };
             }
 
-<<<<<<< HEAD
             return {
                 messages: messages.filter((item) => item.id !== id)
             };
@@ -176,6 +91,7 @@ export default class Chat extends React.Component {
             let newMessages = messages.map((message) => {
                 if (message.id === id) {
                     message.text = inputText;
+                    message.edited = true;
                     return message;
                 }
                 return message;
@@ -207,55 +123,5 @@ export default class Chat extends React.Component {
                             messageToEditId={this.state.messageToEditId}/>
             </div>
         );
-=======
-=======
->>>>>>> 0ce8f22... Add editing message functionality
-            return {
-                messages: messages.filter((item) => item.id !== id)
-            };
-        });
-    }
-    
-    editMessage = (id) => {
-        this.setState(({ messages, inputText }) => {
-            let newMessages = messages.map((message) => {
-                if (message.id === id) {
-                    message.text = inputText;
-                    return message;
-                }
-                return message;
-            })
-
-            return {
-                messages: newMessages,
-                inputText: '',
-                messageToEditId: null
-            }
-        });
-    }
-
-    onEditedMessageSubmit = (event) => {
-        event.preventDefault();
-        this.editMessage(this.state.messageToEditId);
-    }
-
-    render() {
-        return (
-            <div className='chat'>
-                <Messages messages={this.state.messages}
-                          onEditMessage={this.inputMessageToEdit}
-                          onDeleteMessage={this.deleteMessage}/>
-                <InputPanel onTextInput={this.onTextInput}
-                            onTextSubmit={this.onTextSubmit}
-                            onEditedMessageSubmit={this.onEditedMessageSubmit}
-                            text={this.state.inputText}
-                            messageToEditId={this.state.messageToEditId}/>
-            </div>
-<<<<<<< HEAD
-        )
->>>>>>> afb5b5f... Add project
-=======
-        );
->>>>>>> 743f4cf... Add Messages componenet
     }
 }
--- src/Components/Messages/Message/Message.js
+++ src/Components/Messages/Message/Message.js
@@ -5,7 +5,7 @@ import './Message.css';
 export default class Message extends React.Component {
 
     state = {
-        liked: false,
+        liked: false
     }
 
     likeClick = () => {
@@ -19,13 +19,10 @@ export default class Message extends React.Component {
     render() {
         const { name, text, image, ownMessage,
                 onEditMessage, onDeleteMessage,
-                id, time } = this.props;
+                id, time, edited } = this.props;
         const { liked } = this.state;
+        const editedNote = edited ? '(edited)' : null;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> b6558b5... Change own messages appearance
         if (ownMessage) {
             return (
                 <div className='message '>
@@ -34,7 +31,7 @@ export default class Message extends React.Component {
                         <p className='text'>{text}</p>
                         <p className='tools'>
                             <div className='info'>
-                                <span className='time'>{time}</span>
+                                <span className='time'>{time} {editedNote}</span>
                                 {/*<span className='likes-count'>likes</span>*/}
                             </div>
 
@@ -51,35 +48,11 @@ export default class Message extends React.Component {
                 </div>
             );
         }
-<<<<<<< HEAD
-=======
-        const ownIcons = (
-            <div className='icons'>
-                <img src='/images/edit.png' alt=''
-                        onClick={() => onEditMessage(id)}/>
-                <img src='/images/trash.webp' alt=''
-                        onClick={() => onDeleteMessage(id)}/>
-            </div>
-        );
->>>>>>> 743f4cf... Add Messages componenet
-=======
->>>>>>> b6558b5... Change own messages appearance
 
         let heartImg = liked ?
             '/images/heart-black.svg' :
             '/images/heart-thin.svg';
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-        let icons = ownMessage ? ownIcons :
-            (<img className='like' src={heartImg} alt=''
-                  onClick={this.likeClick}/>);
-
-
->>>>>>> 743f4cf... Add Messages componenet
-=======
->>>>>>> b6558b5... Change own messages appearance
         return (
             <div className='message'>
                 <img className='avatar' src={image} alt={name}/>
@@ -87,23 +60,10 @@ export default class Message extends React.Component {
                 <div className='message-box'>
                     <span className='nickname'>{name}</span>
                     <p className='text'>{text}</p>
-<<<<<<< HEAD
-<<<<<<< HEAD
                     <p className='tools'>
-                        <span className='time'>{time}</span>
+                        <span className='time'>{time} {editedNote}</span>
                         <img className='like' src={heartImg} alt=''
                              onClick={this.likeClick}/>
-=======
-                    <p className='info'>
-                        <span className='time'>{time}</span>
-                        <span className='icons'>{icons}</span>
->>>>>>> 743f4cf... Add Messages componenet
-=======
-                    <p className='tools'>
-                        <span className='time'>{time}</span>
-                        <img className='like' src={heartImg} alt=''
-                             onClick={this.likeClick}/>
->>>>>>> b6558b5... Change own messages appearance
                     </p>
                 </div>
             </div>
